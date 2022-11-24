f2edf88ad8c2dbdcb3b04b7a8bc75b729ae9b524
bakhtkhilji23 <bakhtkhilji23@gmail.com>
2022-11-24 11:48:49 +0500

    commit

--- lib/views/lets_go.dart
+++ lib/views/lets_go.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
 import 'package:tourism/utils/style/style.dart';
 import 'package:tourism/views/navigation.dart';
